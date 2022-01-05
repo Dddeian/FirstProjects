1c63868944f5475971b099380e4952ae03079b18
Dddeian <92357148+Dddeian@users.noreply.github.com>
2022-01-05 23:33:23 +0200

    Add files via upload

--- number_Guess.py
+++ number_Guess.py
@@ -1,7 +1,7 @@
 import random
 
 class GuessTheNumber:
-    def __init__(self,min,max,yNum,rNUm,guess,guesses,pCh):
+    def __init__(self,min,max,yNum,rNUm,guess,guesses,pCh,number):
         self.yNum = yNum
         self.min = min
         self.max = max
@@ -9,66 +9,70 @@ class GuessTheNumber:
         self.guess = None
         self.guesses = 1
         self.pCh = None
+        self.number = None
+
 
     def preset(self):
-        print('Please select a range:\n1 => 0 - 10\n2 => 0 - 25\n3 => 0 - 50\n4 => 0 - 100\n5 => 0 - 200\n6 => 0 - 250\n7 => 0 - 500\n8 => 0 - 750\n9 => 0 - 1000\n10 => 0 - 2000')
-        self.pCh = int(input('Type 1 - 10,corresponding to each range -- '))
+        print(50*str('*') + '\nPlease select a range:\n1 => 0 - 10\n2 => 0 - 25\n3 => 0 - 50\n4 => 0 - 100\n5 => 0 - 200\n6 => 0 - 250\n7 => 0 - 500\n8 => 0 - 750\n9 => 0 - 1000\n10 => 0 - 2000')
+        self.pCh = int(input(50*str('*') + '\nType 1 - 10,corresponding to each range -- '))
         if self.pCh == 1:
-            print('Guess the number 0 - 10')
+            print(50*str('*') + '\nGuess the number 0 - 10\n')
             self.min = 0
             self.max = 10
         elif self.pCh == 2:
-            print('Guess the number 0 - 25')
+            print(50*str('*') + '\nGuess the number 0 - 25\n')
             self.min = 0
             self.max = 25
         elif self.pCh == 3:
-            print('Guess the number 0 - 50')
+            print(50*str('*') + '\nGuess the number 0 - 50\n')
             self.min = 0
             self.max = 50
         elif self.pCh == 4:
-            print('Guess the number 0 - 100')
+            print(50*str('*') + '\nGuess the number 0 - 100\n')
             self.min = 0
             self.max = 100
         elif self.pCh == 5:
-            print('Guess the number 0 - 200')
+            print(50*str('*') + '\nGuess the number 0 - 200\n')
             self.min = 0
             self.max = 200
         elif self.pCh == 6:
-            print('Guess the number 0 - 250')
+            print(50*str('*') + '\nGuess the number 0 - 250\n')
             self.min = 0
             self.max = 250
         elif self.pCh == 7:
-            print('Guess the number 0 - 500')
+            print(50*str('*') + '\nGuess the number 0 - 500\n')
             self.min = 0
             self.max = 500
         elif self.pCh == 8:
-            print('Guess the number 0 - 750')
+            print(50*str('*') + '\nGuess the number 0 - 750\n')
             self.min = 0
             self.max = 750
         elif self.pCh == 9:
-            print('Guess the number 0 - 1000')
+            print(50*str('*') + '\nGuess the number 0 - 1000\n')
             self.min = 0
             self.max = 1000
         elif self.pCh == 10:
-            print('Guess the number 0 - 2000')
+            print(50*str('*') + '\nGuess the number 0 - 2000\n')
             self.min = 0
             self.max = 2000
         while self.pCh > 10:
-            print('Please enter a valid value,1 - 10')
+            print(50*str('*') + '\nPlease enter a valid value,1 - 10')
             return self.presetOver10()
     
+
     def yourGuessPreset(self):
         while True:
-            self.guess = int(input('Take a guess here --- '))
+            self.guess = input('\nTake a guess here --- ')
             try:
-                None
-            except:
-                continue
-
+                self.guess = int(self.guess)
+            except ValueError:
+                print(50*str('*') + '\nEnter a NUMBER!')
+                return self.yourGuessPreset()
+                
             if self.guess < self.rNum:
-                print(f'{self.guess} is LOWER than the number.')
+                print(50*str('*') + f'\n{self.guess} <--- than the number.')
             elif self.guess > self.rNum:
-                print(f'{self.guess} is HIGHER than the number.')
+                print(50*str('*') + f'\n{self.guess} ---> than the number.')
             else:
                 break
             self.guesses += 1
@@ -182,108 +186,124 @@ class GuessTheNumber:
             elif self.guesses > 19 and self.guesses <  25:
                 print(f'You succesfully guessed the number in {self.guesses} guesse.Not a bad score!')
             elif self.guesses > 24:
-                print(f'You succssefully guessed the number in {self.guesses} guessses.You can do better!')
-    
+                print(f'You succssefully guessed the number in {self.guesses} guessses.You can do better')
+   
+   
     def presetOver10(self):
-        self.pCh = int(input('Type 1 - 10,corresponding to each range -- '))
+        self.pCh = int(input(50*str('*') + '\nType 1 - 10,corresponding to each range -- '))
         if self.pCh == 1:
-            print('Guess the number 0 - 10')
+            print(50*str('*') + '\nGuess the number 0 - 10')
             self.min = 0
             self.max = 10
         elif self.pCh == 2:
-            print('Guess the number 0 - 25')
+            print(50*str('*') + '\nGuess the number 0 - 25')
             self.min = 0
             self.max = 25
         elif self.pCh == 3:
-            print('Guess the number 0 - 50')
+            print(50*str('*') + '\nGuess the number 0 - 50')
             self.min = 0
             self.max = 50
         elif self.pCh == 4:
-            print('Guess the number 0 - 100')
+            print(50*str('*') + '\nGuess the number 0 - 100')
             self.min = 0
             self.max = 100
         elif self.pCh == 5:
-            print('Guess the number 0 - 200')
+            print(50*str('*') + '\nGuess the number 0 - 200')
             self.min = 0
             self.max = 200
         elif self.pCh == 6:
-            print('Guess the number 0 - 250')
+            print(50*str('*') + '\nGuess the number 0 - 250')
             self.min = 0
             self.max = 250
         elif self.pCh == 7:
-            print('Guess the number 0 - 500')
+            print(50*str('*') + '\nGuess the number 0 - 500')
             self.min = 0
             self.max = 500
         elif self.pCh == 8:
-            print('Guess the number 0 - 750')
+            print(50*str('*') + '\nGuess the number 0 - 750')
             self.min = 0
             self.max = 750
         elif self.pCh == 9:
-            print('Guess the number 0 - 1000')
+            print(50*str('*') + '\nGuess the number 0 - 1000')
             self.min = 0
             self.max = 1000
         elif self.pCh == 10:
-            print('Guess the number 0 - 2000')
+            print(50*str('*') + '\nGuess the number 0 - 2000')
             self.min = 0
             self.max = 2000
         while self.pCh > 10:
-            print('Please enter a valid value,1 - 10')
+            print(50*str('*') + '\nPlease enter a valid value,1 - 10')
             return self.presetOver10()
 
+
     def manual(self):
-        self.min = int(input('Insert minimum value here: '))
-        self.max = int(input('Insert maximum value here: '))
-        while self.min >= self.max:
-            print('The MINIMUM value has to be LOWER than the MAXIMUM value.')
-            return self.manual()        
-            
+        self.min = int(input('Minumum: '))
+        self.max = int(input('Maximum: '))
+        print(f'Guess a number {self.min}-{self.max}')
+        while int(self.min) >= int(self.max):
+            print(50*str('*') + '\nThe MINIMUM value has to be LOWER than the MAXIMUM value.')
+            return self.manual()    
+    
+
     def yourGuessManual(self):
-        while True:
-            self.guess = int(input('Take a guess here --- '))
-            try:
-                None
-            except:
-                continue
-
-            if self.guess < self.rNum:
-                print(f'{self.guess} is LOWER than the number.')
-            elif self.guess > self.rNum:
-                print(f'{self.guess} is HIGHER than the number.')
+            while True:
+                self.guess = input('\nTake a guess --- ')
+                try:
+                    self.guess = int(self.guess)
+                except ValueError:
+                    print(50*str('*') + '\nEnter a NUMBER!')
+                    return self.yourGuessManual()
+                    
+                if self.guess < self.rNum:
+                    print(50*str('*') + f'\n{self.guess} <--- than the number.')
+                elif self.guess > self.rNum:
+                    print(50*str('*') + f'\n{self.guess} ---> than the number.')
+                else:
+                    break
+                self.guesses += 1
+    
+            if self.guesses == 1:
+                print(f'\nCongrats!It took you {self.guesses} guess to succsesfully guess the number.\n' + 50*str('*'))
             else:
-                break
-            self.guesses += 1
-
-        if self.guesses == 1:
-            print(f'Congrats!It took you {self.guesses} guess to succsesfully guess the number.')
-        else:
-            print(f'Congrats!It took you {self.guesses} guesses to succsesfully guess the number.')
+                print(f'\nCongrats!It took you {self.guesses} guesses to succsesfully guess the number.\n' + 50*str('*'))
    
+
     def randomInRangeManual(self):
         self.manual()
-        self.rNum = random.randint(self.min,self.max+1)
+        self.rNum = random.randint(self.min,self.max)
         self.yourGuessManual()
+
 
     def randomInRnagePreset(self):
         self.preset()
-        self.rNum = random.randint(self.min,self.max+1)
+        self.rNum = random.randint(self.min,self.max)
         self.yourGuessPreset()
+
 
     def play(self): 
         while True:
-            print('Choose to manually set range,or use preset by typing "manual" or "preset".')
+            print(50*str('*') + '\nChoose to manually set range,or use preset by typing "manual" or "preset".')
             choice = str(input('Type here: ')).lower()
             manual_Set = 'manual' 
             manual_Set2 = 'manually'
             preset_Set = 'preset'
             if choice == manual_Set or choice == manual_Set2:
-                print('Choose your range: min and max')
+                print(50*str('*') + '\nChoose your range: min and max')
                 self.randomInRangeManual()
-                break
+                if self.min == str('stop'):
+                    break
+                else:
+                    print('\nhttps://youtu.be/bqLNdXS7YFU\n')
+                    break
             elif choice == preset_Set:
-                self.randomInRnagePreset()      
+                self.randomInRnagePreset()  
+                print('\nhttps://youtu.be/bqLNdXS7YFU\n') 
+                break
+            elif choice == str('stop') or str('break'):
                 break
             else:
-                print('Please enter a valid value.')
+                print(50*str('*') + '\nPlease enter a valid value.')
 
-game = GuessTheNumber(0,0,0,0,0,0,0)
+
+game = GuessTheNumber(0,0,0,0,0,0,0,0)
 game.play()
